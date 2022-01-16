7df93906a607e7210ddda011d00094295cb4ff01
sebastian rebolledo <srebolledodiaz@gmail.com>
2022-01-15 23:04:00 -0300

    Pruebas hito 1 ML terminadas

---
+++ Prueba Machine Learning/func_analitics.py
@@ -0,0 +1,7 @@
+def suma(num1, num2):
+    """
+    En la siguiente funcion entran 2 numeros: num1 y num2, los cuales se suman y 
+    la salida entrega la suma del resultado.
+    """
+    return num1+num2
+
